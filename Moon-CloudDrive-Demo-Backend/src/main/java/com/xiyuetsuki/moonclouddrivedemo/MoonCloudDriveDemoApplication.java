e63c557b27d444c0fdf396add974cbd649b9a572
XiyueTsuki <2731867801@qq.com>
2026-08-16 15:35:06 +0800

    init

---
+++ Moon-CloudDrive-Demo-Backend/src/main/java/com/xiyuetsuki/moonclouddrivedemo/MoonCloudDriveDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xiyuetsuki.moonclouddrivedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonCloudDriveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonCloudDriveDemoApplication.class, args);
+	}
+
+}
---
+++ Moon-CloudDrive-Demo-Backend/src/test/java/com/xiyuetsuki/moonclouddrivedemo/MoonCloudDriveDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiyuetsuki.moonclouddrivedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoonCloudDriveDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
